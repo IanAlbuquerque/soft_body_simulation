58beb4db7f30e61636b5f1e94ba0d93907de09bc
Victor Souza <victoraugustolls@gmail.com>
2016-12-17 19:24:42 -0200

    List

--- 2D/src/linalg.cpp
+++ 2D/src/linalg.cpp
@@ -4,3 +4,18 @@ void f()
 {
 	printf("f was called\n");
 }
+
+typedef struct Vertice {
+	int no;
+	double massa;
+	double posicao;
+	Viz ** vizinhos;
+
+};
+
+typedef struct Viz
+{
+	int no;
+	double mola;
+	Viz *prox;
+};
