bb2c8bdd08f5a86bdfb8b6e03b67950a3bb09c9f
Ian Albuquerque <ian.albuquerque.silva@gmail.com>
2016-12-18 15:02:56 -0200

    init vertices

--- 2D/include/simulation.h
+++ 2D/include/simulation.h
@@ -7,4 +7,6 @@ typedef struct _Vertex Vertex;
 
 void simulate(Vertex* list_of_vertices, int num_vertices, double time_to_simulate);
 
+void initVertices(Vertex* list_of_vertices, int* num_vertices, char* file_name);
+
 #endif
