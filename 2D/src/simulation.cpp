261d3556e9a8d136d6a7d8eb8788277a31ac2f24
Ian Albuquerque <ian.albuquerque.silva@gmail.com>
2016-12-18 15:45:07 -0200

    derivative begin implementation

--- 2D/src/simulation.cpp
+++ 2D/src/simulation.cpp
@@ -14,6 +14,7 @@ struct _Vertex {
 	int *neighbours;
 	double *coeff_k;
 	double *rest_r;
+	int num_neighbours;
 };
 
 void initVertices(Vertex* list_of_vertices, int* num_vertices, char* file_name)
@@ -27,7 +28,7 @@ void initVertices(Vertex* list_of_vertices, int* num_vertices, char* file_name)
 	Vector velocity(2);
 	position[0] = 0.5;
 	position[1] = 0.5;
-	Vertex one = { .node = 0, .mass = 0, .position = position, .velocity = velocity, .neighbours = NULL, .coeff_k = NULL, .rest_r = NULL };
+	Vertex one = { .node = 0, .mass = 0, .position = position, .velocity = velocity, .neighbours = NULL, .coeff_k = NULL, .rest_r = NULL, .num_neighbours = 1 };
 
 	list[0] = one;
 
@@ -41,4 +42,33 @@ Vector getPosition(Vertex* list_of_vertex, int num_vertex)
 	position[0] = 0.5;
 	position[1] = 0.5;
 	return position;
+}
+
+void derivative(	Vector& d_position
+					Vector& d_velocity,
+					int vertex_index,
+					Vertex* list_of_vertices,
+					int* num_vertices,
+					double gravity)
+{
+	Vector force(2);
+	Vertex v = list_of_vertices[vertex_index];
+	Vector d;
+	double d_norm;
+	int neighbour_index;
+	Vertex neighbour;
+
+	force[0] = 0;
+	force[1] = gravity;
+
+	for(int i=0; i<v->num_neighbours; i++)
+	{
+		neighbour_index = v->neighbours[i];
+		neighbour = list_of_vertices[neighbour_index];
+		d = neighbour->position - v->position;
+		force += -v->coeff_k[i] * ( - v->rest_r[i])
+	}
+
+	d_position = v->velocity;
+	d_velocity = force / v->mass;
 }
