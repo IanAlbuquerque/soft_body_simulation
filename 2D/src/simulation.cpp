40b4f18255d324a842afe499101c3f6a9fc6c19f
Ian Albuquerque <ian.albuquerque.silva@gmail.com>
2016-12-18 17:00:01 -0200

    runge kuta

--- 2D/src/simulation.cpp
+++ 2D/src/simulation.cpp
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include "simulation.h"
 
+#define GRAVITY_CTE 10.0
+
 void f()
 {
 	printf("f was called\n");
@@ -60,11 +62,12 @@ void printVertices(Vertex* list_of_vertices, int num_vertices)
 	}
 }
 
-void derivative(	Vector& d_position,
-					Vector& d_velocity,
+void derivative(	Vector position,
+					Vector velocity,
+					Vector* d_position,
+					Vector* d_velocity,
 					int vertex_index,
 					Vertex* list_of_vertices,
-					int* num_vertices,
 					double gravity)
 {
 	Vector force(2);
@@ -81,11 +84,110 @@ void derivative(	Vector& d_position,
 	{
 		neighbour_index = v.neighbours[i];
 		neighbour = list_of_vertices[neighbour_index];
-		d = *(neighbour.position) - *(v.position);
+		d = *(neighbour.position) - position;
 		d_norm = d.norm();
 		force += -v.coeff_k[i] * ( d_norm - v.rest_r[i]) * d / d_norm;
 	}
 
-	d_position = (*v.velocity);
-	d_velocity = force / v.mass;
+	*d_position = velocity;
+	*d_velocity = force / v.mass;
+}
+
+void simulate(Vertex* list_of_vertices, int num_vertices, double time_to_simulate)
+{
+	Vector** new_positions = new Vector*[num_vertices];
+	Vector** new_velocities = new Vector*[num_vertices];
+
+	Vector position(2);
+	Vector velocity(2);
+	Vector d_position(2);
+	Vector d_velocity(2);
+
+	double h = time_to_simulate;
+
+	for(int i=0; i<num_vertices; i++)
+	{
+		new_positions[i] = new Vector(2);
+		new_velocities[i] = new Vector(2);
+
+		*new_positions[i] = 0;
+		*new_velocities[i] = 0;
+
+		position = *(list_of_vertices[i].position);
+		velocity = *(list_of_vertices[i].velocity);
+
+		// --------------- step 1
+
+		derivative(	position,
+					velocity,
+					&d_position,
+					&d_velocity,
+					i,
+					list_of_vertices,
+					GRAVITY_CTE);
+		position = *(list_of_vertices[i].position) + h*d_position;
+		velocity = *(list_of_vertices[i].position) + h*d_velocity;
+
+		*new_positions[i] += position;
+		*new_velocities[i] += velocity;
+
+		// --------------- step 2
+
+		derivative(	position,
+					velocity,
+					&d_position,
+					&d_velocity,
+					i,
+					list_of_vertices,
+					GRAVITY_CTE);
+		position = *(list_of_vertices[i].position) + h/2.0*d_position;
+		velocity = *(list_of_vertices[i].position) + h/2.0*d_velocity;
+
+		*new_positions[i] += 2*position;
+		*new_velocities[i] += 2*velocity;
+
+		// --------------- step 3
+
+		derivative(	position,
+					velocity,
+					&d_position,
+					&d_velocity,
+					i,
+					list_of_vertices,
+					GRAVITY_CTE);
+		position = *(list_of_vertices[i].position) + h/2.0*d_position;
+		velocity = *(list_of_vertices[i].position) + h/2.0*d_velocity;
+
+		*new_positions[i] += 2*position;
+		*new_velocities[i] += 2*velocity;
+
+		// --------------- step 4
+
+		derivative(	position,
+					velocity,
+					&d_position,
+					&d_velocity,
+					i,
+					list_of_vertices,
+					GRAVITY_CTE);
+		position = *(list_of_vertices[i].position) + h*d_position;
+		velocity = *(list_of_vertices[i].position) + h*d_velocity;
+
+		*new_positions[i] += position;
+		*new_velocities[i] += velocity;
+
+		*new_positions[i] /= 6.0;
+		*new_velocities[i] /= 6.0;
+	}
+
+	for(int i=0; i<num_vertices; i++)
+	{
+		delete list_of_vertices[i].position;
+		delete list_of_vertices[i].velocity;
+		list_of_vertices[i].position = new_positions[i];
+		list_of_vertices[i].position = new_velocities[i];
+	}
+
+	delete new_positions;
+	delete new_velocities;
 }
