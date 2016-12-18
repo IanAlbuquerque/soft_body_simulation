c97cf582e70f78fa07e2f09f6e9a83e3ff6f4c14
Victor Souza <victoraugustolls@gmail.com>
2016-12-18 15:23:28 -0200

    Sample vertex

--- 2D/include/simulation.h
+++ 2D/include/simulation.h
@@ -1,12 +1,14 @@
-#ifndef MATRIX_H
-#define MATRIX_H
+#ifndef SIMULATION_H
+#define SIMULATION_H
 
 #include "Vector.h"
 
 typedef struct _Vertex Vertex;
 
+void initVertices(Vertex* list_of_vertices, int* num_vertices, char* file_name);
+
 void simulate(Vertex* list_of_vertices, int num_vertices, double time_to_simulate);
 
-void initVertices(Vertex* list_of_vertices, int* num_vertices, char* file_name);
+Vector getPosition(Vertex* list_of_vertex, int num_vertex);
 
 #endif
